3efc6cdf52216b91763ef66f1d812d6d44e48321
Youssef Sanad <yosefsanad11@gmail.com>
2021-01-11 14:16:29 +0200

    adding Cors middleware and removing teh JWT for testing purposes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\Cors;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
@@ -78,6 +80,7 @@ $app->configure('app');
 
  $app->routeMiddleware([
      'auth' => App\Http\Middleware\Authenticate::class,
+     'cors' => Cors::class
  ]);
 
 /*
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('register', 'AuthController@register');
     $router->post('login', 'AuthController@login');
 
-    $router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->group(['middleware' => 'cors'], function () use ($router) {
         $router->get('/', function () use ($router) { return $router->app->version(); });
 
         //Job Posts APIs
